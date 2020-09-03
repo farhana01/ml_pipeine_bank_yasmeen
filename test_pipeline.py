a2839c8785a659c465d32432f1f2fa277321c259
farhana01 <farhanalima01@gmail.com>
2020-09-03 12:19:59 +0600

    demo project

---
+++ test_pipeline.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 10:38:41 2020
+
+@author: User
+"""
+
---
+++ train_pipeline.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 10:38:13 2020
+
+@author: User
+"""
+
