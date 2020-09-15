4230ad7dcbaadaeb9c44bf37ea0f3282d19a177b
farhana01 <farhanalima01@gmail.com>
2020-09-15 09:50:56 +0600

    string conversion

--- train_pipeline.py
+++ train_pipeline.py
@@ -53,4 +53,47 @@ print(train_data.isnull().sum())
 
 train_data.info()
 #vizualizing outliers
-sns.boxplot(data=train_data[''])
+sns.set(style="whitegrid")
+ax=sns.boxplot(data=train_data["age"])
+
+#remove outliers
+from scipy import stats
+thresh=3
+train_data=train_data[np.abs(stats.zscore(train_data["age"]))<thresh]
+#vizualizing outliers
+sns.set(style="whitegrid")
+ax=sns.boxplot(data=train_data["age"])
+
+train_data=train_data[np.abs(stats.zscore(train_data["balance"]))<thresh]
+train_data=train_data[np.abs(stats.zscore(train_data["day"]))<thresh]
+train_data=train_data[np.abs(stats.zscore(train_data["duration"]))<thresh]
+train_data=train_data[np.abs(stats.zscore(train_data["campaign"]))<thresh]
+train_data=train_data[np.abs(stats.zscore(train_data["pdays"]))<thresh]
+train_data=train_data[np.abs(stats.zscore(train_data["previous"]))<thresh]
+
+
+
+#string conversion , label encoding, one hot encoder
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+#train_data['marital'] = le.fit_transform(train_data['marital'])
+train_data['loan'] = le.fit_transform(train_data['loan'])
+train_data['class'] = le.fit_transform(train_data['class'])
+train_data['housing'] = le.fit_transform(train_data['housing'])
+train_data.info()
+train_data['loan'].head()
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer (transformers = [('encoder', OneHotEncoder(),[2,3, 8])],remainder='passthrough')
+train_data = np.array(ct.fit_transform(train_data))
+train_data
+#why should we transform it to numpy arrayt? 
+#train_data is numpy array now not a dataframe, will it be a problem?
+#train_data.info()
+train_data=pd.DataFrame(train_data)
+train_data.head()
+train_data.info()
+
+
